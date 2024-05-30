61fa62535125735b7b27939e5f8959e90cb4d8ad
Matthew Tighe <matthew.e.tighe@gmail.com>
2024-05-29 18:42:55 -0700

    Update build script with snapshot instructions

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
+# To make snapshots of a running container issue the following command:
+# docker commit <docker ID from docker PS command> <new name>
+# then either change the name of the image here or remove old image & re-tag 
+# docker image im <old name>
+# docker tag <new name> <old name>
+# docker rmi <new name>
+# 
 docker build --rm -f Dockerfile -t almalinux-dev .
