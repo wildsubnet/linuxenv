9d6ff253875efa27bfb4d2a1d1080ea1d50afb63
Matthew Tighe <matthew.e.tighe@gmail.com>
2024-05-30 17:05:21 -0700

    Update run script with variable for user name and instructions for VSC

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
-docker run -it --user devuser -v "/Users/user/Documents/vmshare":/vmshare almalinux-dev /bin/zsh
+#
+# run.sh <local user>
+# run as background process if attaching remotely using VSC or similar tool
+#
+docker run -it --user devuser -v "/Users/$1/Documents/vmshare":/vmshare -w /vmshare almalinux-dev /bin/zsh
 
